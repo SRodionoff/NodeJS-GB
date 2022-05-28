78617e14a7cbb36718fc3c07ee6b2ef916b5a389
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-05-28 04:40:38 +0300

    Update cliapp.js

--- cliapp.js
+++ cliapp.js
@@ -8,33 +8,28 @@ inquirer
         {
             name: "directory",
             type: "file-tree-selection",
-            message: "Choose directory",
+            message: "Выберите директорию",
         },
+        {
+            name: "fir_search",
+            type: "input",
+            message:"Введите IP:",
+        },  
     ])
     .then((answer) => {
         const filePath = answer.directory;
         const readStream = new fs.ReadStream(filePath, 'utf8');
-        const writeStream1 = fs.createWriteStream('./89.123.1.41_requests.log', {
-            flags: "a",
-            encoding: "utf-8",
-        });
-        const writeStream2 = fs.createWriteStream('./34.48.240.111_requests.log', {
+        const writeStream1 = fs.createWriteStream(`./%${answer.fir_search}.log`, {
             flags: "a",
             encoding: "utf-8",
         });
         const { Transform } = require('stream');
         const transformStream1 = new Transform({
             transform(chunk, encoding, callback) {
-                const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
+                const re = new RegExp(`^${answer.fir_search} .*`, 'gm')
+                const transformedChunk = chunk.toString().match(re).join('\n');
                 callback(null, transformedChunk);
             }
         });
-        const transformStream2 = new Transform({
-            transform(chunk, encoding, callback) {
-                const transformedChunk = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
-                callback(null, transformedChunk);
-            }
-        });
-        readStream.pipe(transformStream1).pipe(writeStream1);
-        readStream.pipe(transformStream2).pipe(writeStream2);
+        readStream.pipe(transformStream1).pipe(writeStream1);        
     });
